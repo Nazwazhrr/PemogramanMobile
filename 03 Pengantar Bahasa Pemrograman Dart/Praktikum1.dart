713856030f2fae8b3aaee0afd2f095053b55e74c
nazwazhrr <nazwaazahra883@gmail.com>
2026-03-05 22:15:54 +0700

    Pertemuan 3 Dart

--- 03 Pengantar Bahasa Pemrograman Dart/Praktikum1.dart
+++ 03 Pengantar Bahasa Pemrograman Dart/Praktikum1.dart
@@ -11,7 +11,12 @@ void main() {
 
   if (test == "test2") print("Test2 again");
 
-bool testBaru = true;
+  // String test = "true";
+  // if (test) {
+  //   print("Kebenaran");
+  // }
+
+  bool testBaru = true;
 
   if (testBaru) {
     print("Kebenaran");
