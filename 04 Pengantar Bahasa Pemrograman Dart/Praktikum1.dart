a6de2f0612c87ee301a4948b6273458366f4908b
nazwazhrr <nazwaazahra883@gmail.com>
2026-03-28 13:46:10 +0700

    Pertemuan 4 Dart

--- 04 Pengantar Bahasa Pemrograman Dart/Praktikum1.dart
+++ 04 Pengantar Bahasa Pemrograman Dart/Praktikum1.dart
@@ -1,12 +1,21 @@
+// void main() {
+//   var list = [1, 2, 3];
+//   assert(list.length == 3);
+//   assert(list[1] == 2);
+
+//   print(list.length);
+//   print(list[1]);
+
+//   list[1] = 1;
+//   assert(list[1] == 1);
+//   print(list[1]);
+// }
+
 void main() {
-  var list = [1, 2, 3];
-  assert(list.length == 3);
-  assert(list[1] == 2);
+  final List<String?> list = List.filled(5, null);
 
-  print(list.length);
-  print(list[1]);
+  list[1] = "Nazwa Azahra Audina";
+  list[2] = "244107060146";
 
-  list[1] = 1;
-  assert(list[1] == 1);
-  print(list[1]);
+  print(list);
 }
