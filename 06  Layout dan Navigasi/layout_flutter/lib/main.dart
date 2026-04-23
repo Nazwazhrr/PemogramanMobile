e9a958f84400dd6fb5f56880dbf24c61b4fa9289
nazwazhrr <nazwaazahra883@gmail.com>
2026-04-23 22:50:59 +0700

    Pertemuan 6 Layout dan Navigasi

---
+++ 06  Layout dan Navigasi/layout_flutter/lib/main.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    Widget titleSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: const Text(
+                    'Wisata Gunung di Batu',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                const Text(
+                  'Batu, Malang, Indonesia',
+                  style: TextStyle(
+                    color: Colors.grey,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const Icon(
+            Icons.star,
+            color: Colors.red,
+          ),
+          const Text('41'),
+        ],
+      ),
+    );
+
+    Color color = Theme.of(context).primaryColor;
+
+Widget buttonSection = Row(
+  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+  children: [
+    _buildButtonColumn(color, Icons.call, 'CALL'),
+    _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+    _buildButtonColumn(color, Icons.share, 'SHARE'),
+  ],
+);
+
+  Widget textSection = Container(
+    padding: const EdgeInsets.all(32),
+    child: const Text(
+      'Gunung di kawasan Batu, Malang merupakan salah satu destinasi wisata '
+      'alam yang populer di Jawa Timur. Daerah ini menawarkan pemandangan '
+      'pegunungan yang indah, udara yang sejuk, serta berbagai aktivitas '
+      'outdoor seperti hiking dan camping. Banyak wisatawan datang untuk '
+      'menikmati keindahan alam serta spot foto yang menarik.\n\n'
+      'Nama: Nazwa Azahra Audina\n'
+      'NIM: 244107060146',
+      softWrap: true,
+    ),
+  );
+
+  return MaterialApp(
+    title: 'Flutter layout: Nazwa azahra Audina 244107060146',
+    home: Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter layout demo'),
+      ), // AppBar
+      body: ListView(
+        children: [
+          Image.asset(
+            'images/cangar.jpg',
+            width: 600,
+            height: 240,
+            fit: BoxFit.cover,
+          ), // Image.asset
+          titleSection,
+          buttonSection,
+          textSection,
+        ], // ListView children
+      ), // ListView
+    ), // Scaffold
+  ); // MaterialApp
+}
+
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
